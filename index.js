8b47d0ab5cc53fb9becf95651dba0fb1ab8e3124
wunpun <wunpun3@gmail.com>
2025-11-19 17:53:52 +0300

    Functions Lab

--- index.js
+++ index.js
@@ -5,3 +5,39 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateTax(amount) {
+    const taxValue = amount * (10/100)
+    return taxValue
+}
+
+function convertToUpperCase(text) {
+    const convertedText = text.toUpperCase();
+    return convertedText
+}
+
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    }
+    else {
+        return num2
+    }
+}
+
+function isPalindrome(word) {
+    const characterArray = word.split('');
+    const reversedArray = characterArray.reverse();
+    const reversedWord = reversedArray.join('');
+    if (reversedWord === word) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountedPrice = originalPrice * (1 - discountPercentage/100)
+    return discountedPrice 
+}
